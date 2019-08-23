5c0d8e26f24906aec957be75e18ad2a9f121c994
Aleksey Abramov <develop@webburov.com>
2019-08-23 17:08:18 +0300

    add program

--- themes/onair/assets/js/program.js
+++ themes/onair/assets/js/program.js
@@ -1,10 +1,18 @@
 (function ($) {
     $.ajax({
-        url: 'https://dev19.mir24.tv/api/smart/v1/channels',
-        type: 'POST',
+        url: '//dev19.mir24.tv/api/smart/v1/channels',
+        type: 'GET',
         dataType: 'json',
         success: function(data) {
-            console.log(data);
+            let broadcastsToday = data[0].broadcasts.filter(item => dateFns.isToday(new Date(item.time.begin)));
+            let index = broadcastsToday.findIndex(item => (new Date(item.time.begin).getTime()) >= Date.now()) - 1;
+            let result = broadcastsToday.slice(index);
+            $('.program .in').empty();
+            result.forEach(item => $('.program .in').append('<div class="program__item">' +
+                '<div class="program__time">' + dateFns.format(item.time.begin, 'HH:mm') + '</div>' +
+                '<div class="program__name">' + item.title + '</div>' +
+                '</div>'));
+            $('.program').addClass('loaded').find('i').fadeOut(150);
         }
     });
 })(jQuery);
