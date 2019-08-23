38f7abe0d4bc3cda4540f1a7253ad95fc210612e
Aleksey Abramov <develop@webburov.com>
2019-08-23 10:40:18 +0300

    add custom plagin

---
+++ plugins/wadwettay/blog/updates/create_posts_table.php
@@ -0,0 +1,22 @@
+<?php namespace Wadwettay\Blog\Updates;
+
+use Schema;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+
+class CreatePostsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('wadwettay_blog_posts', function(Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('wadwettay_blog_posts');
+    }
+}
