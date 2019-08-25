8902be3b64c47bd69862bf9efec441debebd949f
Aleksey Abramov <develop@webburov.com>
2019-08-25 11:18:06 +0300

    fix player

--- themes/onair/assets/js/program.js
+++ themes/onair/assets/js/program.js
@@ -7,6 +7,8 @@
             let broadcastsToday = data[0].broadcasts.filter(item => dateFns.isToday(new Date(item.time.begin)));
             let index = broadcastsToday.findIndex(item => (new Date(item.time.begin).getTime()) >= Date.now()) - 1;
             let result = broadcastsToday.slice(index);
+            $('span.onair__subtitle').empty().html(result[0].title);
+            result = result.slice(1)
             $('.program .in').empty();
             result.forEach(item => $('.program .in').append('<div class="program__item">' +
                 '<div class="program__time">' + dateFns.format(item.time.begin, 'HH:mm') + '</div>' +
--- themes/onair/assets/js/trinsling.js
+++ themes/onair/assets/js/trinsling.js
@@ -1,81 +1,52 @@
 (function ($) {
-    $('.channels .channel').on('click', function () {
-        let that = $(this);
-        if(that.is('.channel--active')) return;
-        let id = that.data('id');
-        $('.channels .channel').removeClass('channel--active');
-        that.addClass('channel--active');
-        $('.trinsling-tabs .tab').removeClass('active');
-        $('.trinsling-tabs .tab[data-index="' + id + '"]').addClass('active');
-    });
 
     $(document).ready(function() {
-        // $('.translation .video').each(function () {
-        //     let playerId = 'player-' + $(this).data('rand');
-        //     let stream = $(this).data('stream');
-        //     if(Hls.isSupported())
-        //     {
-        //         let video = document.getElementById(playerId);
-        //         let hls = new Hls();
-        //         hls.loadSource(stream);
-        //         hls.attachMedia(video);
-        //         hls.on(Hls.Events.MANIFEST_PARSED,function()
-        //         {
-        //             video.play();
-        //         });
-        //     }
-        //     else if (video.canPlayType('application/vnd.apple.mpegurl'))
-        //     {
-        //         video.src = stream;
-        //         video.addEventListener('canplay',function()
-        //         {
-        //             video.play();
-        //         });
-        //     }
-        // });
-    //
-        let dat = 0;
-        if(dat == 4){
-            p.play();
-            $("#radio").show();
-            $("#container").hide();
-        } else {
-            $('.translation .video').each(function () {
-                let playerId = 'html5player-' + $(this).data('rand');
-                let stream = $(this).data('stream');
-                let player = new Plyr(playerId, {
-                    /* options */
-                });
 
-                player.source = {
-                    type: 'video',
-                    title: 'Example title',
-                    sources: [
-                        {
-                            src: stream,
+        $('.translation .video').each(function () {
+            let playerId = 'html5player-' + $(this).data('rand');
+            let stream = $(this).data('stream');
+            if(Modernizr.video){
+                $(this).html("<video id='" + playerId + "' class='html5video video-js vjs-default-skin' controls autoplay>" +
+                    "<source src=\"" +stream + "\" type='application/x-mpegurl' >" +
+                    "</video>");
+                let player = videojs(playerId);
+                $(this).data('player', player);
+            } else {
+                $(this).data('player', flowplayer(playerId,{
+                    swf : "//flowplayer.swf",
+                    clip: {
+                        sources: [{
                             type: 'application/x-mpegurl',
-                            // size: 720,
-                        }
-                    ]
-                };
-                // if(Modernizr.video){
-                //     $(this).html("<video id='" + playerId + "' class='html5video video-js vjs-default-skin' controls autoplay>" +
-                //         "<source src=\"" +stream + "\" type='application/x-mpegurl' >" +
-                //         "</video>")
-                //     let player = videojs(playerId);
-                //     player.play();
-                // } else {
-                //     let flw = flowplayer(playerId,{
-                //         swf : "//flowplayer.swf",
-                //         clip: {
-                //             sources: [{
-                //                 type: 'application/x-mpegurl',
-                //                 src: stream
-                //             }]
-                //         }
-                //     });
-                // }
+                            src: stream
+                        }]
+                    }
+                }));
+            }
+        });
+
+        $('.channels .channel').on('click', function () {
+            let that = $(this);
+            if(that.is('.channel--active')) return;
+            let id = that.data('id');
+            $('.channels .channel').removeClass('channel--active');
+            that.addClass('channel--active');
+            $('.trinsling-tabs .tab').removeClass('active');
+            $('.trinsling-tabs .tab[data-index="' + id + '"]').addClass('active');
+            $('.translation .video').each(function () {
+                $(this).data('player').pause();
             });
-        }
+            $('.trinsling-tabs .tab[data-index="' + id + '"]').find('.video').data('player').play();
+
+        });
+
+        function getSong() {
+            $.get('//radiomir.fm/radiometa.php').done((res) => {
+                if(res) {
+                    $('span.player__name').empty().html(res);
+                }
+            });
+        };
+        getSong();
+        setInterval(getSong,3000);
     });
 })(jQuery);
